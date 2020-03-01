60b7f7e29baa43403b208ced3fbc1f14d4a2f59e
m2727864 <60123226+m2727864@users.noreply.github.com>
2020-02-29 20:17:04 -0700

    Add files via upload

---
+++ Function in javascript/index.js
@@ -0,0 +1,59 @@
+function mandeep(name1,name2){
+    return name1+name2;
+  }
+  console.log (mandeep("john","tom"))
+  
+  
+  function maths(p1,p2,p3){
+    return p1+p2+p3;
+  }
+  console.log (mandeep(2,3,5))
+  
+  
+  function hello(phase1,phase2){
+    return hello("Hello" + phase1 + phase2);
+  }
+  console.log (hello("google","yahoo"))
+  
+  
+  function subtraction(a,b){
+    return a-b;
+  }
+  console.log (subtraction(5,3))
+  
+  
+  function football(mike,trevor){
+    return mike + " is playing with " +trevor;
+  }
+  console.log (football("mike","trevor"))
+  
+  
+  function addition(n1,n2){
+    return n1+n2;
+  }
+  console.log (addition(4,6))
+  
+  
+  function product(c1,c2){
+    return c1*c2;
+  }
+  console.log (product(4,3))
+  
+  
+  function relationship(franklin,daisy){
+    return franklin+ "is dating" +daisy;
+  }
+  console.log (relationship("franklin","daisy"))
+  
+  
+  function division (term1,term2){
+    return term1/term2;
+  }
+  console.log (division(4,2))
+  
+  
+  function playstation(player1,player2){
+    return player1+ "is playing" +player2;
+  }
+  console.log (playstation("player1","player2"))
+  
