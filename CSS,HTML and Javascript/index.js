5b56a76fbcf866e0b006c6137f586c877a319c5a
m2727864 <60123226+m2727864@users.noreply.github.com>
2020-03-11 12:25:29 -0600

    Add files via upload

---
+++ CSS,HTML and Javascript/index.js
@@ -0,0 +1,22 @@
+const maximun = document.getElementById("display");
+const singh = document.getElementById("exhibit"); 
+const image = document.getElementById("link");
+const href = link.Attribute("href");
+
+//const display = document.querySelector(".display");
+
+//start.addEventListener("click", startFn);
+
+//clear.addEventListener("click", clearFn);
+maximun.addEventListener("click",function(){
+maximun.textContent="Hello mandeep";})
+
+singh.addEventListener("click",function(){
+singh.textContent="Welcome to Tech1101";
+
+image.addEventListener("click",function(){
+image.textContent="image";    
+})
+
+
+})
